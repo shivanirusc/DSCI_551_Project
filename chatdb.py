a60dba4f95bac7bc1e68aa55116bde283493ed8d
shivanirusc <166767362+shivanirusc@users.noreply.github.com>
2024-10-12 02:57:20 -0700

    Update chatdb.py

--- chatdb.py
+++ chatdb.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1cFmOYrj0S0pMGvIcB_nAuQkuolNBlPTN
 """
 
-pip install streamlit
-
 import streamlit as st
 
 # Title and description
