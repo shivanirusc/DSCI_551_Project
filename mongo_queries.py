3f22e6f236138e2bb3272d1335a4b1fd88805006
Haley Boren <hboren@usc.edu>
2024-11-20 20:03:14 -0800

    Add documentation to functions and clean up code

--- mongo_queries.py
+++ mongo_queries.py
@@ -44,26 +44,42 @@ def store_in_mongodb(data, json_name):
     mongodb_list.append(collectionName)
     return collectionName
 
-def view_mongodb_head(collectionName):
-    collection = mongo_db[collectionName]
-    # Fetch the first 5 documents
-    documents = list(collection.find().limit(5))
-    for idx, doc in enumerate(documents, start=1):
-        print(f"Document {idx}: {doc}")
-    print(f"Displayed {len(documents)} document(s).")
-
 def infer_types(df_):
+    """Infers the types of each column and returns the different groups of columns, and ensures 
+        column names don't have any punctuation or spaces for later querying
+
+    Parameters
+    ----------
+    df_ : pandas dataframe
+        The dataframe that will be ingested to either mongodb or sqlite
+
+    Returns
+    -------
+    numeric_cols
+        a list of strings used that are the header columns
+    categorical_cols
+        a list of strings used that are the header columns
+    nested_cols
+        a list of strings used that are the header columns
+    unique_cols
+        a list of strings used that are the header columns
+    df_
+        dataframe with the updated column names
+    """
     # add functionality for columns that only have unique values
     numeric_cols = []
     categorical_cols = []
     nested_cols = []
     unique_cols = []
 
+    # remove any punctuation that may hinder querying later
     df_.columns = [
         col.translate(str.maketrans("", "", string.punctuation)).replace(" ", "_")
         for col in df_.columns
     ]
     
+    # for each column, determine if it is numeric, nested, or categorical, and for categorical if
+    # it is a unique value
     for col in df_.columns:
         if df_[col].isna().all():
             continue
@@ -82,13 +98,27 @@ def infer_types(df_):
                 unique_cols.append(col)
             else:
                 categorical_cols.append(col)
-    print("NUMERIC COLS", numeric_cols)
-    print("CATEGORICAL COLS", categorical_cols)
-    print("NESTED COLS", nested_cols)
-    print("UNIQUE COLS", unique_cols)
+    # print("NUMERIC COLS", numeric_cols)
+    # print("CATEGORICAL COLS", categorical_cols)
+    # print("NESTED COLS", nested_cols)
+    # print("UNIQUE COLS", unique_cols)
     return numeric_cols, categorical_cols, nested_cols, unique_cols, df_
 
 def get_quant_range(df_, quant_cols):
+    """Gets the range of each quantitative column
+
+    Parameters
+    ----------
+    df_ : pandas dataframe
+        The dataframe that will be ingested to either mongodb or sqlite
+    quant_cols : list
+        List of the quantitative variable names
+
+    Returns
+    -------
+    range_dict
+        Dictionary where the key is the quantitative column and the value is a tuple containing the (min, max) value for said column
+    """
     range_dict = {}
     for col in quant_cols:
         min_val = df_[col].min()
@@ -128,6 +158,18 @@ def get_mongo_user_input():
     print(None)
 
 def gen_total_query(cat_cols, quant_cols, collectionName):
+    """Generates a query in the Total <A> for each <B> Category, where A is a random quantitative variable and B is a 
+        random categorical variable, prints this query in natural language and mongo format, and prints the head(5) of the output
+
+    Parameters
+    ----------
+    cat_cols : list
+        The dataframe that will be ingested to either mongodb or sqlite
+    quant_cols : list
+        List of the quantitative variable names
+    collectionName : str
+        Name of the collection we are currently querying on
+    """
     random_quant = random.choice(quant_cols)
     random_cat = random.choice(cat_cols)
     random_quant_var = f"Total {random_quant}"
@@ -153,6 +195,18 @@ def gen_total_query(cat_cols, quant_cols, collectionName):
     
 
 def gen_average_query(cat_cols, quant_cols, collectionName):
+    """Generates a query in the Average <A> for each <B> Category, where A is a random quantitative variable and B is a 
+        random categorical variable, prints this query in natural language and mongo format, and prints the head(5) of the output
+
+    Parameters
+    ----------
+    cat_cols : list
+        The dataframe that will be ingested to either mongodb or sqlite
+    quant_cols : list
+        List of the quantitative variable names
+    collectionName : str
+        Name of the collection we are currently querying on
+    """
     random_quant = random.choice(quant_cols)
     random_cat = random.choice(cat_cols)
     random_quant_var = f"Average {random_quant}"
@@ -174,6 +228,16 @@ def gen_average_query(cat_cols, quant_cols, collectionName):
     print(tabulate(rows, headers=headers, tablefmt="grid"))
 
 def gen_counts_query(cat_cols, collectionName):
+    """Generates a query in the Count for each <B> Category, where A is a random quantitative variable and B is a 
+        random categorical variable, prints this query in natural language and mongo format, and prints the head(5) of the output
+
+    Parameters
+    ----------
+    cat_cols : list
+        The dataframe that will be ingested to either mongodb or sqlite
+    collectionName : str
+        Name of the collection we are currently querying on
+    """
     random_cat = random.choice(cat_cols)
     nat_language = f"Counts for each {random_cat} category"
     collection = mongo_db[collectionName]
@@ -191,20 +255,42 @@ def gen_counts_query(cat_cols, collectionName):
     print("Below is the result of the query:")
     print(tabulate(rows, headers=headers, tablefmt="grid"))
 
-def gen_greater_query_group(cat_cols, quant_cols, unique_cols, range_, collectionName):
-    cat_combined = cat_cols + unique_cols
+def gen_gtlt_query_group(cat_cols, quant_cols, range_, ineq, collectionName):
+    """Generates a query in the <B> with Average <A> Greater/Less than X, where A is a random quantitative variable and B is a 
+        random categorical variable, and X is a random value within the range of the random quantitative column, and
+        prints this query in natural language and mongo format, and prints the head(5) of the output
+
+    Parameters
+    ----------
+    cat_cols : list
+        The dataframe that will be ingested to either mongodb or sqlite
+    quant_cols : list
+        List of the quantitative variable names
+    range_ : dictionary
+         Dictionary where the key is the quantitative column and the value is a tuple containing the (min, max) value for said column
+    ineq : str
+        Name of the ineq that we are using (gt or lt)
+    collectionName : str
+        Name of the collection we are currently querying on
+    """
+    if ineq == 'gt':
+        ineq_str = "greater"
+    elif ineq == 'lt':
+         ineq_str = "less"
+    else:
+        ineq_str = "unknown"
     random_quant = random.choice(quant_cols)
     random_quant_var = f"Average {random_quant}"
-    random_cat = random.choice(cat_combined)
-    gt_val = random.randint(range_[random_quant][0], range_[random_quant][1])
-    nat_language = f"{random_cat} with {random_quant} greater than {gt_val}"
+    random_cat = random.choice(cat_cols)
+    gt_val = random.randint(int(range_[random_quant][0]), int(range_[random_quant][1]))
+    nat_language = f"{random_cat} with {random_quant_var} {ineq_str} than {gt_val}"
     collection = mongo_db[collectionName]
     query_result = collection.aggregate([{"$group": {"_id": f"${random_cat}", random_quant_var: {"$avg": f"${random_quant}"}}}, 
-                                         {"$match": {random_quant_var: {"$gt": gt_val}}},
+                                         {"$match": {random_quant_var: {f"${ineq}": gt_val}}},
                                          {"$project": {f"{random_cat}": "$_id", random_quant_var: 1, "_id": 0}}
                                          ,{"$limit": 5}])
     query = [{"$group": {"_id": f"${random_cat}", random_quant_var: {"$avg": f"${random_quant}"}}}, 
-                                         {"$match": {random_quant_var: {"$gt": gt_val}}},
+                                         {"$match": {random_quant_var: {f"${ineq}": gt_val}}},
                                          {"$project": {f"{random_cat}": "$_id", random_quant_var: 1, "_id": 0}}
                                          ,{"$limit": 5}]
     query_string = ', '.join(json.dumps(part) for part in query)
@@ -225,7 +311,6 @@ df = pd.read_json(f'data/{filename}')
 # make sure quantitative variables are in numeric type
 numeric, categorical, nested, unique, df_updated = infer_types(df)
 range_vals = get_quant_range(df, numeric)
-print("HEIGHT MIN",range_vals['Height_m'][0])
 # store updated df in mongodb
 collection_name = store_in_mongodb(df_updated, filename)
 
@@ -235,6 +320,7 @@ sample_tokens = process_input("Please provide sample queries")
 gen_total_query(categorical, numeric, collection_name)
 gen_average_query(categorical, numeric, collection_name)
 gen_counts_query(categorical, collection_name)
-gen_greater_query_group(categorical, numeric, unique, range_vals, collection_name)
+gen_gtlt_query_group(categorical, numeric, range_vals, "gt", collection_name)
+gen_gtlt_query_group(categorical, numeric, range_vals, "lt", collection_name)
 # generate sample queries with a specific construct, ie. group by
 # generate query based on matching template to 
