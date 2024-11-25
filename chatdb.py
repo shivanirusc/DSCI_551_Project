27d7cde1f4049dfb8a5f27f1e6f24116e1ac46bb
shivanirusc <166767362+shivanirusc@users.noreply.github.com>
2024-11-24 17:10:25 -0800

    Update chatdb.py

--- chatdb.py
+++ chatdb.py
@@ -243,6 +243,7 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import os
+import re
 
 from sql_sample_queries import generate_sample_queries
 
